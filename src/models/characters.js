e8056b904513fc6e0ff34e11fa50aac6c23610b5
Thomas Richner <thomasrichner+github@gmail.com>
2015-08-18 04:08:10 +0200

    code formattting

--- src/models/characters.js
+++ src/models/characters.js
@@ -1,70 +1,71 @@
 /* jshint indent: 2 */
 
-module.exports = function(sequelize, DataTypes) {
-  var characters = sequelize.define('characters', {
-    characterID: {
-      type: DataTypes.INTEGER(11),
-      allowNull: false
-    },
-    charName: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    fUserID: { /* Forum User ID */
-      type: DataTypes.INTEGER(11),
-      allowNull: false
-    },
-    accountID: {
-      type: DataTypes.INTEGER(11),
-      allowNull: false
-    },
-    primaryChar: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false
-    },
-    firstPull: {
-      type: DataTypes.DATE,
-      allowNull: false
-    },
-    latestPull: {
-      type: DataTypes.DATE,
-      allowNull: false
-    },
-    charSheetCache: {
-      type: DataTypes.DATE,
-      allowNull: false
-    }
-  }, {
-    timestamps: false,
-    classMethods: {
-      associate: function(models) {
+module.exports = function (sequelize, DataTypes) {
+    var characters = sequelize.define('characters', {
+        characterID: {
+            type: DataTypes.INTEGER(11),
+            allowNull: false
+        },
+        charName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        fUserID: {
+            /* Forum User ID */
+            type: DataTypes.INTEGER(11),
+            allowNull: false
+        },
+        accountID: {
+            type: DataTypes.INTEGER(11),
+            allowNull: false
+        },
+        primaryChar: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false
+        },
+        firstPull: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        latestPull: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        charSheetCache: {
+            type: DataTypes.DATE,
+            allowNull: false
+        }
+    }, {
+        timestamps: false,
+        classMethods: {
+            associate: function (models) {
 
-      }
-    },
-    instanceMethods: {
-      addLinkedCharacter: function (linkedId) {
-        return sequelize.query("INSERT INTO `linkedcharacters` VALUES (:characterID,:linkedTo)",
-            {
-              replacements: {
-                characterID: this.characterID,
-                linkedTo: linkedId
-              },
-              type: sequelize.QueryTypes.INSERT
-            })
-      },
-      getLinkedCharacters: function () {
-        return sequelize.query("SELECT * FROM `characters` C INNER JOIN" +
-            " `linkedcharacters` L ON C.characterID=L.linkedTo WHERE L.characterID =" +
-            " :characterID ",
-            {
-              replacements: {
-                characterID: this.characterID
-              },
-              model: characters, // FIXME does this work?
-              type: sequelize.QueryTypes.SELECT
-            })
-      }
-    }
-  });
-  return characters;
+            }
+        },
+        instanceMethods: {
+            addLinkedCharacter: function (linkedId) {
+                return sequelize.query("INSERT INTO `linkedcharacters` VALUES (:characterID,:linkedTo)",
+                    {
+                        replacements: {
+                            characterID: this.characterID,
+                            linkedTo: linkedId
+                        },
+                        type: sequelize.QueryTypes.INSERT
+                    })
+            },
+            getLinkedCharacters: function () {
+                return sequelize.query("SELECT * FROM `characters` C INNER JOIN" +
+                    " `linkedcharacters` L ON C.characterID=L.linkedTo WHERE L.characterID =" +
+                    " :characterID ",
+                    {
+                        replacements: {
+                            characterID: this.characterID
+                        },
+                        model: characters, // FIXME does this work?
+                        type: sequelize.QueryTypes.SELECT
+                    })
+            }
+        }
+    });
+    return characters;
 };
